8dbab526035c46f6e9a7f9ea5cf85bb02e0bbe2d
iareuniqe <1062698930@qq.com>
2017-11-13 00:36:53 +0800

    add two java

---
+++ kafka/src/com/kafka/Launcher.java
@@ -0,0 +1,15 @@
+/**
+ * 2017Äê11ÔÂ13ÈÕLauncherDELL_pc
+ */
+package com.kafka;
+
+/**
+ * @author DELL_pc
+ *
+ */
+public class Launcher {
+
+	public static void main(String[] args) {
+		System.out.println("this is launcher");
+	}
+}
---
+++ kafka/src/com/kafka/Main.java
@@ -0,0 +1,16 @@
+/**
+ * 2017Äê11ÔÂ13ÈÕMainDELL_pc
+ */
+package com.kafka;
+
+/**
+ * @author DELL_pc
+ *
+ */
+public class Main {
+	
+	public static void main(String[] args) {
+		System.out.println("world");
+	}
+
+}
